1f70b768e9c65d2c9e44a61bfb9f545d973b2607
evrmgzm <isci.gizem35@gmail.com>
2025-05-10 18:31:28 +0300

    Update index.php

--- backend/index.php
+++ backend/index.php
@@ -1,9 +1,12 @@
 <?php
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type');
-
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 /**
  * API URL'sinden kullanıcı verilerini çeker
  * @return array Kullanıcı verileri dizisi
